2f6d13574d8de694992019c34f16477e406e7db0
Thijs Brobbel <thijsbrobbel@gmail.com>
2015-04-14 22:47:09 +0200

    Add integration test about whitespace.

---
+++ BrainfuckInterpreter.IntegrationTests/WhitespaceTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace BrainfuckInterpreter.IntegrationTests
+{
+    public class WhitespaceTests
+    {
+        [Fact]
+        public void ProgramData_WithWhitespace_ShouldNotBang()
+        {
+            // arrange
+            const string programText = "hoi";
+
+            var program = Encoding.ASCII.GetBytes(programText);
+
+            var outputStream = new MemoryStream();
+            var outputWriter = new StreamWriter(outputStream);
+            var sut = new Interpreter(outputWriter);
+
+            // act
+            Action act = () =>
+                sut.ParseAndExecute(program);
+
+            // assert
+            act.ShouldNotThrow();
+        }
+    }
+}

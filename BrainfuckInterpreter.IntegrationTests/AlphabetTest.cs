6ea94ad3fb404ce64549d3df7edaad2f79c24c34
Thijs Brobbel <thijsbrobbel@gmail.com>
2015-04-14 23:04:23 +0200

    Add more complicated alphabet program,
    which contains a loop.

--- BrainfuckInterpreter.IntegrationTests/AlphabetTest.cs
+++ BrainfuckInterpreter.IntegrationTests/AlphabetTest.cs
@@ -33,5 +33,38 @@ namespace BrainfuckInterpreter.IntegrationTests
             outputStream.Position = 0;
             Encoding.ASCII.GetString(outputStream.ToArray()).Should().Be("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
         }
+
+        [Fact]
+        public void Alphabet()
+        {
+            // arrange
+            const string programText =
+                // cell 0 = 26
+                "++++++++++++++++++++++++++" +
+                // cell 1 = 65
+                ">+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++<" +
+                // while cell#0 != 0
+                "[" +
+                // move to cell 1, output it, and increment it
+                 ">.+" +
+                 // move back to cell 0, and decrement it
+                 "<-" +
+                 // end loop (actually jump back to start of loop)
+                "]";
+
+            var program = Encoding.ASCII.GetBytes(programText);
+
+            var outputStream = new MemoryStream();
+            var outputWriter = new StreamWriter(outputStream);
+            var sut = new Interpreter(outputWriter);
+
+            // act
+            sut.ParseAndExecute(program);
+
+            // assert
+            outputWriter.Flush();
+            outputStream.Position = 0;
+            Encoding.ASCII.GetString(outputStream.ToArray()).Should().Be("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        }
     }
 }

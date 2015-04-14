108feca33e2a462d9caff981dbf058b359424864
Thijs Brobbel <thijsbrobbel@gmail.com>
2015-04-14 21:32:51 +0200

    Initial commit

---
+++ BrainfuckInterpreter/Interpreter.cs
@@ -0,0 +1,10 @@
+﻿namespace BrainfuckInterpreter
+{
+    internal class Interpreter
+    {
+        public void ParseAndExecute(byte[] program)
+        {
+            
+        }
+    }
+}
---
+++ BrainfuckInterpreter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace BrainfuckInterpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var programFile = args.First();
+            var program = File.ReadAllBytes(programFile);
+
+            new Interpreter().ParseAndExecute(program);
+        }
+    }
+}

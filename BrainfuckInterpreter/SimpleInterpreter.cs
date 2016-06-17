129d4ce98d50c5a2c15d927a32888af5b97b10bf
Thijs Brobbel <t.brobbel@coolblue.nl>
2016-06-17 16:51:14 +0200

    Add initial version of the simple interpreter.

---
+++ BrainfuckInterpreter/SimpleInterpreter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace BrainfuckInterpreter
+{
+    public class SimpleInterpreter
+    {
+        private readonly TextWriter _outputWriter;
+
+        public SimpleInterpreter(TextWriter outputWriter)
+        {
+            _outputWriter = outputWriter;
+        }
+
+        public void ParseAndExecute(byte[] program)
+        {
+            var instructionPointer = 0;
+            var dataPointer = 0;
+            var data = new byte[30000];
+
+            var programLength = program.Length;
+
+            while(instructionPointer < programLength)
+            {
+                char instruction = '0'; //program[instructionPointer];
+                switch (instruction)
+                {
+                    case '<':
+                        dataPointer--;
+                        break;
+                    case '>':
+                        dataPointer++;
+                        break;
+                    case '-':
+                        data[dataPointer]--;
+                        break;
+                    case '+':
+                        data[dataPointer]++;
+                        break;
+                    case ',':
+                        data[dataPointer] = (byte)Console.Read();
+                        break;
+                    case '.':
+                        _outputWriter.Write((char)data[dataPointer]);
+                        break;
+                    case '[':
+                        // TODO: jump forward, if data[dataPointer] == 0
+                        break;
+                    case ']':
+                        // TODO: jump backwards, if data[dataPointer] != 0
+                        break;
+
+                        // rest is a no op
+                }
+                instructionPointer++;
+            }
+
+        }
+    }
+}
